49ac4831d92192780d3b9b8281004cbf2dc7e6a2
Ẏṏṳçḕf MặđSķůłl <32908247+madskullkoa@users.noreply.github.com>
2021-09-07 15:04:31 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import random
 
+nombre=random.randint(0,36)
+capital=1000
+mise=int(input("Entre la mise initiale : "))
+capital=capital-mise
+n=0
+while(capital>0):
+	nombre=random.randint(0,36)
+	if(nombre%2 == 0):
+		print("gg")
+		capital =+ mise * 2
+	elif (nombre==0 or nombre%2 != 0):
+		print("perdu")
+	print(nombre)
+	n=+1
+
+print("T'as perdu au bout de ",n,)
