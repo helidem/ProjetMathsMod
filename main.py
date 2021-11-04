54709a2e22a53e15231bc0d26a01e8243921bd90
Youcef <you.neuffdeuxx@gmail.com>
2021-11-04 13:10:32 +0100

    Ajout méthode fibo et mise fixe

--- main.py
+++ main.py
@@ -1,33 +1,88 @@
 import random
-import numpy as np
-#definition des variables globales
-NBIT = 500
-choix = 'pair'
+import statistics
 
+# definition des variables globales
+NBPARIS = 500
+NBIT = 10000
+choix = 'pair'
 miseDep = 20
 valeurs = ["pair", "impair", "0"]
-proba = [18/37,18/37,1/37]
-mise=miseDep
-capitaux=np.array()
-print("Martingale classique")
-for j in range(10000):
-	
-	capitalCumule=0
-	capital=1000
-	for i in range(NBIT):
+proba = [18 / 37, 18 / 37, 1 / 37]
 
-		capital=capital-mise
-		if(capital<=0):
-			#print("Plus d'argent au bout de", i,"iterations")
-			break
-		resultat = random.choices(valeurs,proba)
-		if(resultat[0] == 'pair'):
-			capital=capital+(mise*2)
-			mise=miseDep
-			#print("G", mise, capital)
-		elif(resultat[0] == 'impair'):
-			mise=mise*2
-			#print("P",mise, capital)	
-	capitaux.add(capital)
-moyenne=capitaux.mean(capitaux,axis=None)
-print(moyenne)
+print("############ Martingale classique ############")
+capitaux = []
+faillites = 0
+for j in range(NBIT):
+    capital = 1000
+    mise = miseDep
+    for i in range(NBPARIS):
+        if capital - mise <= 0:
+            faillites = faillites + 1
+            break
+        capital = capital - mise
+        resultat = random.choices(valeurs, proba)
+        if resultat[0] == 'pair':
+            capital = capital + (mise * 2)
+            mise = miseDep
+        else:
+            mise = mise * 2
+    capitaux.append(capital)
+moyenne = statistics.mean(capitaux)
+print("La moyenne des capitaux avec la méthode de la martingale est de ", moyenne, " et le maximum est", max(capitaux), ". le pourcentage de bénéfices est de ", 100-(faillites*100/NBIT),"%")
+############
+
+
+print("############ Fibonacci ############")
+capitaux = []
+faillites = 0
+for j in range(NBIT):
+    capital = 1000
+
+    mises = [miseDep]
+    idx = 0
+    mise = sum(mises)
+    for i in range(NBPARIS):
+        if capital - mise <= 0:
+            faillites = faillites + 1
+            break
+        capital = capital - mise
+        resultat = random.choices(valeurs, proba)
+        if resultat[0] == 'pair':
+            capital = capital + (mise * 2)
+            idx = 0
+            mises = [miseDep]
+            mise = sum(mises)
+        else:
+            if idx == 0:
+                mises.append(miseDep)
+                idx = idx + 1
+                mise = sum(mises)
+            else:
+                mises.append(mises[idx-1] + mises[idx])
+                idx = idx + 1
+                mise = mises[idx-1] + mises[idx]
+    capitaux.append(capital)
+moyenne = statistics.mean(capitaux)
+print("La moyenne des capitaux avec la méthode Fibonacci est de ", moyenne, " et le maximum est", max(capitaux), ". le pourcentage de bénéfices est de ", 100-(faillites*100/NBIT),"%")
+
+############
+
+
+print("############ Mise fixe ############")
+capitaux = []
+faillites = 0
+for j in range(NBIT):
+    capital = 1000
+
+    for i in range(NBPARIS):
+        mise = miseDep
+        if capital - mise <= 0:
+            faillites = faillites + 1
+            break
+        capital = capital - mise
+        resultat = random.choices(valeurs, proba)
+        if resultat[0] == 'pair':
+            capital = capital + (mise * 2)
+    capitaux.append(capital)
+moyenne = statistics.mean(capitaux)
+print("La moyenne des capitaux avec la méthode de la mise fixe est de ", moyenne, " et le maximum est", max(capitaux), ". le pourcentage de bénéfices est de ", 100-(faillites*100/NBIT),"%")
