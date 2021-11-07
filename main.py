74688b3f5cb5c8ce8fc3385c02c0a18118a9728f
Youcef <you.neuffdeuxx@gmail.com>
2021-11-07 11:43:13 +0100

    ajout mise proportionnelle + benefices

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ proba = [18 / 37, 18 / 37, 1 / 37]
 print("############ Martingale classique ############")
 capitaux = []
 faillites = 0
+benefices = 0
 for j in range(NBIT):
     capital = 1000
     mise = miseDep
@@ -26,15 +27,20 @@ for j in range(NBIT):
             mise = miseDep
         else:
             mise = mise * 2
+    if capital > 1000:
+        benefices = benefices + 1
     capitaux.append(capital)
-moyenne = statistics.mean(capitaux)
-print("La moyenne des capitaux avec la méthode de la martingale est de ", moyenne, " et le maximum est", max(capitaux), ". le pourcentage de bénéfices est de ", 100-(faillites*100/NBIT),"%")
+moyenne = statistics.fmean(capitaux)
+print("La moyenne des capitaux avec la méthode de la martingale est de ", moyenne, " et le maximum est", max(capitaux),
+      ". le pourcentage de personnes qui ressortent sans argent est de ", (faillites * 100 / NBIT),
+      "%. Et le pourcentages de personnes qui réalisent des bénéfices est de :", benefices * 100 / NBIT)
 ############
 
 
 print("############ Fibonacci ############")
 capitaux = []
 faillites = 0
+benefices = 0
 for j in range(NBIT):
     capital = 1000
 
@@ -58,12 +64,16 @@ for j in range(NBIT):
                 idx = idx + 1
                 mise = sum(mises)
             else:
-                mises.append(mises[idx-1] + mises[idx])
+                mises.append(mises[idx - 1] + mises[idx])
                 idx = idx + 1
-                mise = mises[idx-1] + mises[idx]
+                mise = mises[idx - 1] + mises[idx]
+    if capital > 1000:
+        benefices = benefices + 1
     capitaux.append(capital)
-moyenne = statistics.mean(capitaux)
-print("La moyenne des capitaux avec la méthode Fibonacci est de ", moyenne, " et le maximum est", max(capitaux), ". le pourcentage de bénéfices est de ", 100-(faillites*100/NBIT),"%")
+moyenne = statistics.fmean(capitaux)
+print("La moyenne des capitaux avec la méthode Fibonacci est de ", moyenne, " et le maximum est", max(capitaux),
+      ". le pourcentage de personnes qui ressortent sans argent est de ", (faillites * 100 / NBIT),
+      "%. Et le pourcentages de personnes qui réalisent des bénéfices est de :", benefices * 100 / NBIT)
 
 ############
 
@@ -71,6 +81,7 @@ print("La moyenne des capitaux avec la méthode Fibonacci est de ", moyenne, " e
 print("############ Mise fixe ############")
 capitaux = []
 faillites = 0
+benefices = 0
 for j in range(NBIT):
     capital = 1000
 
@@ -83,6 +94,34 @@ for j in range(NBIT):
         resultat = random.choices(valeurs, proba)
         if resultat[0] == 'pair':
             capital = capital + (mise * 2)
+    if capital > 1000:
+        benefices = benefices + 1
     capitaux.append(capital)
-moyenne = statistics.mean(capitaux)
-print("La moyenne des capitaux avec la méthode de la mise fixe est de ", moyenne, " et le maximum est", max(capitaux), ". le pourcentage de bénéfices est de ", 100-(faillites*100/NBIT),"%")
+moyenne = statistics.fmean(capitaux)
+print("La moyenne des capitaux avec la méthode de la mise fixe est de ", moyenne, " et le maximum est", max(capitaux),
+      ". le pourcentage de personnes qui ressortent sans argent est de ", (faillites * 100 / NBIT),
+      "%. Et le pourcentages de personnes qui réalisent des bénéfices est de :", benefices * 100 / NBIT)
+
+print("############ Mise proportionnelle ############")
+capitaux = []
+faillites = 0
+benefices = 0
+proportion = 0.1
+for j in range(NBIT):
+    capital = 1000
+    for i in range(NBPARIS):
+        mise = capital * proportion
+        if capital - mise <= 0:
+            faillites = faillites + 1
+            break
+        capital = capital - mise
+        resultat = random.choices(valeurs, proba)
+        if resultat[0] == 'pair':
+            capital = capital + (mise * 2)
+    if capital > 1000:
+        benefices = benefices + 1
+    capitaux.append(capital)
+moyenne = statistics.fmean(capitaux)
+print("La moyenne des capitaux avec la méthode de la mise fixe est de ", moyenne, " et le maximum est", max(capitaux),
+      ". le pourcentage de personnes qui ressortent sans argent est de ", (faillites * 100 / NBIT),
+      "%. Et le pourcentages de personnes qui réalisent des bénéfices est de :", benefices * 100 / NBIT)
